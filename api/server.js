f9487f3e93bf2ff7b071afd5c565faf8328010fe
aabdelre <aabdelre@hamilton.edu>
2023-04-06 22:59:37 +0100

    small fixes

--- api/server.js
+++ api/server.js
@@ -10,8 +10,8 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    useNewUrlParser: true, 
+	useUnifiedTopology: true
 }).then(() => console.log("Connected to MongoDB")).catch(console.error);
 
 app.get('/todos', async (req, res) => {
@@ -38,7 +38,7 @@ app.get('/todo/complete/:id', async (req, res) => {
     todo.complete = !todo.complete;
     todo.save();
     res.json(todo);
-})
+});
 //async function connect() {
 //    try {
 //        await mongoose.connect(url, {
@@ -57,5 +57,11 @@ app.get('/todo/complete/:id', async (req, res) => {
 //}).then(() => console.log("Connected to MongoDB")).catch(console.error);
 
 // Models
+app.put('/todo/update/:id', async (req, res) => {
+    const todo = await Todo.findById(req.params.id);
+    todo.text = req.body.text;
+	todo.save();
+	res.json(todo);
+});
 
 app.listen(4000, () => console.log("Server started on port 4000"));
--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 const API_BASE = "http://localhost:4000";
 
 function App() {
@@ -8,11 +8,11 @@ function App() {
 
     useEffect(() => {
         GetTodos();
-        console.log(todos);
-    }, []) 
+        //console.log(todos);
+    }, []);
 
     const GetTodos = () => {
-        fetch(API_BASE + "/todos")
+        fetch(API_BASE + '/todos')
             .then(res => res.json())
             .then(data => setTodos(data))
             .catch(err => console.error("Caught error loading tasks: ", err));
@@ -32,10 +32,11 @@ function App() {
     }
 
     const deleteTodo = async id => {
-        const data = await fetch(API_BASE + "/todo/delete/" + id, {method:"DELETE"})
+        const data = await fetch(API_BASE + '/todo/delete/' + id, {method:"DELETE"})
             .then(res => res.json());
-
-        setTodos(todos => todos.filter(todo => todo._id !== data._id)) 
+        //if(data.item === undefined) {return};
+        console.log(data);
+        setTodos(todos => todos.filter(todo => todo._id !== data._id));
     }
 
     const addTodo = async () => {
@@ -48,34 +49,36 @@ function App() {
                 text: newTodo
             })
         }).then(res => res.json());
-        console.log(data);
+        //console.log(data);
 
         setTodos([...todos, data]);
         setPopupActive(false);
-        setNewTodo("");
+		setNewTodo("");
     }
 
     return (
         <div className="App">
-            <h1> Welcome, Ahmed </h1>
+            <h1> Welcome, Ahmed! </h1>
             <h4> Your Tasks </h4>
 
             <div className="todos">
-                {todos.map(todo => (
-                    <div className={
-                        "todo " + (todo.complete ? "is-complete":"")
-                    } key={todo._id} onClick={() => completeTodo(todo._id)}>
-                        <div className="checkbox"></div>
-                        <div className="text"> {todo.text} </div> 
-                        <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>x</div>
-                    </div>
-                ))}
-            </div>
+				{todos.length > 0 ? todos.map(todo => (
+					<div className={
+						"todo" + (todo.complete ? " is-complete" : "")
+					} key={todo._id} onClick={() => completeTodo(todo._id)}>
+						<div className="checkbox"></div>
+						<div className="text">{todo.text}</div>
+						<div className="delete-todo" onClick={() => deleteTodo(todo._id)}>X</div>
+					</div>
+				)) : (
+					<p>You currently have no tasks!</p>
+				)}
+			</div>
 
             <div className="addPopup" onClick={() => setPopupActive(true)}> + </div>
             {popupActive ? (
                 <div className="popup">
-                    <div className="closePopup" onClick={() => setPopupActive(false)}>x</div>
+                    <div className="closePopup" onClick={() => setPopupActive(false)}>X</div>
                     <div className="Content">
                         <h3>Add Task</h3>
                         <input 
